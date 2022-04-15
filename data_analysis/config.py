950fcd391f0619f810665c59929b412505bc771b
kartikaggarwal98 <kartikaggarwal98@gmail.com>
2022-04-14 18:41:35 -0700

    added config, helper file

---
+++ data_analysis/config.py
@@ -0,0 +1,8 @@
+lang = 'en'
+
+raw_df_path =f'/data/users/kartik/thesis/data/full_dataset/{lang}.complete.csv'
+
+
+full_data_path = f'/data/users/kartik/thesis/processed_data/{lang}.complete.wordgloss_group.pkl'
+sampled_data_path = f'/data/users/kartik/thesis/processed_data/{lang}.10k_sampled.wordgloss_group.pkl'
+
